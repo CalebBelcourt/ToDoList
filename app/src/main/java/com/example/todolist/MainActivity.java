3333a2eb87ff539c09e2cac6a069243bf01e8533
Caleb <calebbelcourt@hotmail.com>
2019-01-18 16:03:52 -0800

    Changed FAB
    Created activities and fab leads to an activity

--- app/src/main/java/com/example/todolist/MainActivity.java
+++ app/src/main/java/com/example/todolist/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.todolist;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FloatingActionButton t_fab = findViewById(R.id.text_fab);
+        t_fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, TaskActivity.class);
+                MainActivity.this.startActivity(intent);
+            }
+        });
     }
+
+
 }
