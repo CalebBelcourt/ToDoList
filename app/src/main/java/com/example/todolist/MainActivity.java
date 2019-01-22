2d6fc6318ba72706b5e49d11b527268902e7ee35
Caleb <calebbelcourt@hotmail.com>
2019-01-22 11:14:16 -0800

    Pika Pi
    Designed Pikachu interface and designed the task activity

--- app/src/main/java/com/example/todolist/MainActivity.java
+++ app/src/main/java/com/example/todolist/MainActivity.java
@@ -14,6 +14,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         FloatingActionButton t_fab = findViewById(R.id.text_fab);
+        FloatingActionButton v_fab = findViewById(R.id.voice_fab);
+        t_fab.bringToFront();
+        v_fab.bringToFront();
+
+
         t_fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -21,6 +26,14 @@ public class MainActivity extends AppCompatActivity {
                 MainActivity.this.startActivity(intent);
             }
         });
+
+        v_fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //open up text to speech
+            }
+        });
+
     }
 
 
